9657c45b37cc79eea53b4edeb03946b0f391c8cb
jaeyong4536 <jaeyong4536@gmail.com>
2020-12-30 11:07:26 +0900

    class and interface sample

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,43 @@
+/*
+interface Person {
+    name: string;
+    age: number;
+    gender: string;
+}
+const cuckoo: Person = {
+    name: "cuckoo",
+    age: 100,
+    gender: "male"
+};
+*/
+
+class Person {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    constructor(name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+
 const name: string = "cuckoo";
 const age: number = 100;
 const gender: string = "male";
+const cuckoo: Person = new Person(name, age, gender);
 
-const sayHello = (name: string, age: number, gender?: string): string => {
-    return `Hello ${name}! you are ${age}, you are a ${gender}.`;
+const sayHello = (person: Person): string => {
+    return `Hello ${person.name}! you are ${person.age}, you are a ${person.gender}.`;
 }
 
 const consoleLog = (logMessage: string): void => {
     console.log(logMessage);
 }
 
-const logMessage1: string = sayHello(name, age, gender);
-const logMessage2: string = sayHello(name, age);
-
-consoleLog(logMessage1);
-consoleLog(logMessage2);
+const logMessage: string = sayHello(cuckoo);
+consoleLog(logMessage);
 
 // if it's not, error occur.
 export {};
